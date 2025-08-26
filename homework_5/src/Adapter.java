8fc0eaecfa904067ab3f62f5d9cbba0daec6a00e
mnmwqes <nargiza150300@gmail.com>
2025-08-26 08:59:48 +0300

    -

---
+++ homework_5/src/Adapter.java
@@ -0,0 +1,40 @@
+interface TypeC {
+    void connectWithTypeC();
+}
+
+class USBDevice {
+    public void connectWithUSB() {
+        System.out.println("Подключено через USB");
+    }
+}
+
+class USBtoTypeCAdapter implements TypeC {
+    private USBDevice usbDevice;
+
+    public USBtoTypeCAdapter(USBDevice usbDevice) {
+        this.usbDevice = usbDevice;
+    }
+
+    @Override
+    public void connectWithTypeC() {
+        System.out.print("Через адаптер: ");
+        usbDevice.connectWithUSB();
+    }
+}
+
+class Laptop {
+    public void plugIn(TypeC device) {
+        device.connectWithTypeC();
+    }
+}
+
+public class Adapter {
+    public static void main(String[] args) {
+        Laptop laptop = new Laptop();
+        USBDevice mouse = new USBDevice();
+
+        TypeC adapter = new USBtoTypeCAdapter(mouse);
+
+        laptop.plugIn(adapter);
+    }
+}
