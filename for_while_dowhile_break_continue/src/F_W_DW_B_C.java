b6ed29ea842cfe0868d96e22d03f310149463fd2
mnmwqes <nargiza150300@gmail.com>
2025-09-24 04:15:19 +0300

    -

---
+++ for_while_dowhile_break_continue/src/F_W_DW_B_C.java
@@ -0,0 +1,25 @@
+
+public class F_W_DW_B_C {
+    public static void main(String[] args) {
+// Далее for, while, do...while.
+
+        for (;;) {
+        }
+        while () {
+        }
+
+        do {
+
+        } while ();
+
+// Далее break, continue.
+        for (;;) {
+        }
+        while () {
+        }
+
+        do {
+
+        } while ();
+    }
+}
