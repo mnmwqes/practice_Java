ffa455457e80449ba948fe6b9937e21770a76a6f
mnmwqes <nargiza150300@gmail.com>
2025-10-23 05:42:26 +0300

    -

---
+++ aston_intensive/string_compare_1/src/Man.java
@@ -0,0 +1,8 @@
+public class Man {
+    public static void main(String[] args) {
+
+        String s1 = "JavaRush - лучший сайт для изучения Java!";
+        String s2 = new String("JavaRush - лучший сайт для изучения Java!");
+        System.out.println(s1 == s2);
+    }
+}
